549b8ccaf50bd2cc25417f14c373bfca17f9ba50
marcin <71821642+MarcinPloch@users.noreply.github.com>
2022-10-01 00:52:51 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,10 @@ import { greetUser } from '$utils/greet';
 window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'John Doe';
-  greetUser(name);
+      
+  
+  
+   
+   
+   greetUser(name);
 });
