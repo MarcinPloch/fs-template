8f57d715fc1c98348873ae3d6cd801aecd148326
marcin <71821642+MarcinPloch@users.noreply.github.com>
2022-10-01 14:14:27 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { greetUser } from '$utils/greet';
 window.Webflow ||= [];
 window.Webflow.push(() => {
   // const name = 444;
-  const number = 444;
+  const number = 4444;
   greetUser(number);
+
+  const heading = document.querySelector('.h1');
+  if (!heading) {
+    return;
+  }
+  heading.textContent = 'dupa';
 });
